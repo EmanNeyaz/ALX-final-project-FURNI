58d2ab5a50304b802f5c8084ec3b974471138038
EmanNeyaz <emanneyaz0@gmail.com>
2025-01-03 17:40:22 +0200

    the main pages was built

---
+++ furni/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FaShoppingCart, FaUser } from "react-icons/fa"; // Ensure correct import
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <h4>FURNI</h4>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/"}>About US</Link>
+        </li>
+        <li>
+          <Link to={"/products"}>Products</Link>
+        </li>
+        <li>
+          <Link to={"/cart"}>
+            <FaShoppingCart /> Cart
+          </Link>
+        </li>
+        <li>
+          <Link to={"/login"}>
+            <FaUser /> Login
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Navbar;
