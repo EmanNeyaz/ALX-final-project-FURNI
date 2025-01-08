f5559cfb9defaa5555d8b0a9e2b710ceb2708a43
EmanNeyaz <emanneyaz0@gmail.com>
2025-01-08 19:31:05 +0200

    upadting the add to cart function

--- furni/Products/Products.jsx
+++ furni/Products/Products.jsx
@@ -9,70 +9,70 @@ const Products = () => {
       id: 1,
       name: 'kruxo chair',
       price: 100,
-      image: <img src="/react/src/assets/images/Kruzo Chair.png" alt="kruzo" />,
+      image: <img src="/react/src/assets/images/Kruzo.png" alt="kruzo" />,
       count: 1
     },
     {
         id: 2,
         name: 'Ergonomic Chair',
         price: 75,
-        image: <img src="/react/src/assets/images/candle.jpg" alt="Ergonomic" />,
+        image: <img src="/react/src/assets/images/Ergonomic.png" alt="Ergonomic" />,
         count: 2
     },
     {
         id: 3,
         name: 'nordic chair',
         price: 75,
-        image: <img src="/react/src/assets/Nordic CHAIR.png" alt="Nordic" />,
+        image: <img src="/react/src/assets/images/Nordic CHAIR.png" alt="Nordic" />,
         count: 3
     },
     {
         id: 4,
         name: 'sofa',
         price: 150,
-        image: <img src="/react/src/assets/sofa.png" alt="sofa" />,
+        image: <img src="/react/src/assets/images/sofa.png" alt="sofa" />,
         count: 4
     },
     {
         id: 5,
         name: 'wood chair',
         price: 50,
-        image: <img src="/react/src/assets/wood chair.png" alt="wood" />,
+        image: <img src="/react/src/assets/images/wood chair.png" alt="wood" />,
         count: 5
     },
     {
         id: 6,
         name: 'living corner',
         price: 250,
-        image: <img src="/react/src/assets/living corner.jpg" alt="living corner" />,
+        image: <img src="/react/src/assets/images/living corner.jpg" alt="living corner" />,
         count: 6
     },
     {
         id: 7,
         name: 'candle',
         price: 25,
-        image: <img src="/react/src/assets/candle.jpg" alt="candle" />,
+        image: <img src="/react/src/assets/images/candle.jpg" alt="candle" />,
         count: 7
     },
     {
         id: 8,
         name: 'comfe chair',
         price: 100,
-        image: <img src="/react/src/assets/comfe chair.jpg" alt="comfe" />,
+        image: <img src="/react/src/assets/images/comfe chair.jpg" alt="comfe" />,
         count: 8
     },
     {
         id: 9,
         name: 'm couch',
         price: 200,
-        image: <img src="/react/src/assets/modern couch.jpg" alt="m couch" />,
+        image: <img src="/react/src/assets/images/modern couch.jpg" alt="m couch" />,
         count: 9
     },
     {
         id: 10,
         name: 'kitchen',
         price: 300,
-        image: <img src="/react/src/assets/kitchen.jpg" alt="kitchen" />,
+        image: <img src="/react/src/assets/images/kitchen.jpg" alt="kitchen" />,
         count: 10
     }
   ];
@@ -83,7 +83,7 @@ const Products = () => {
       <div className='products d-flex flex-wrap justify-content-center'>
         {products.map((item) => (
           <div key={item.id} className='card m-2'>
-            <img src={`/images/${item.image}`} alt="" className='card-img' style={{ width: "18rem" }} />
+            <img src={`/images/${item.image}`} alt="" className='card-img' style={{ width: "25rem", height:"25rem" }} />
             <div className='card-body'>
               <h3>{item.name}</h3>
               <span>{item.price} $</span>
