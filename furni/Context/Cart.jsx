b777426068634fecc469ba0678a6a8f0477275ac
EmanNeyaz <emanneyaz0@gmail.com>
2025-01-09 15:38:41 +0200

    adding more functions to the cart and it's style

--- furni/Context/Cart.jsx
+++ furni/Context/Cart.jsx
@@ -3,7 +3,7 @@ import { CartContext } from './CartContext';
 import './Cart.css';
 
 const Cart = () => {
-  const { cart, removeFromCart, getTotalPrice } = useContext(CartContext);
+  const { cart, removeFromCart, increaseQuantity, decreaseQuantity, getTotalPrice } = useContext(CartContext);
 
   return (
     <div className='cart container'>
@@ -13,7 +13,9 @@ const Cart = () => {
           <tr>
             <th className='border p-2'>Product</th>
             <th className='border p-2'>Price</th>
-            <th className='border p-2'>Remove</th>
+            <th className='border p-2'>Quantity</th>
+            <th className='border p-2'>Total</th>
+            <th className='border p-2'>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -21,6 +23,12 @@ const Cart = () => {
             <tr key={item.id}>
               <td className='border p-2'>{item.name}</td>
               <td className='border p-2'>{item.price} $</td>
+              <td className='border p-2'>
+                <button className='btn btn-secondary' onClick={() => decreaseQuantity(item.id)}>-</button>
+                {item.quantity}
+                <button className='btn btn-secondary' onClick={() => increaseQuantity(item.id)}>+</button>
+              </td>
+              <td className='border p-2'>{item.price * item.quantity} $</td>
               <td className='border p-2'>
                 <button className='btn btn-danger' onClick={() => removeFromCart(item.id)}>Remove</button>
               </td>
